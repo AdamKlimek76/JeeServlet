697f12803522d5479f5896f9eb1dbc5bba34b947
Adam Klimek <a.klim@op.pl>
2020-02-04 19:42:04 +0100

    Zadanie dzien 4 1-3

---
+++ src/main/java/ZadanieDzien4/Servlet311.java
@@ -0,0 +1,49 @@
+package ZadanieDzien4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/servlet311")
+public class Servlet311 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            request.setAttribute("greeting", "Czesc");
+        } else {
+            boolean isCookieExist = false;
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("language")) {
+                    request.setAttribute("greeting", cookie.getValue());
+                    isCookieExist = true;
+                    break;
+                }
+            }
+            if (!isCookieExist) {
+                request.setAttribute("greeting", "Czesc");
+            }
+        }
+
+        Map<String, String> langs = new HashMap<>();
+        langs.put("en", "Hello");
+        langs.put("pl", "Czesc");
+        langs.put("de", "Hallo");
+        langs.put("es", "Hola");
+        langs.put("fr", "Salut");
+
+        request.setAttribute("langs", langs);
+
+        getServletContext().getRequestDispatcher("/ZadanieDzien4/servlet311.jsp")
+                .forward(request, response);
+    }
+}
